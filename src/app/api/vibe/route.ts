e2cc26823bba60b7c0c199b8d26808d80e7ac1ae
Harshit Sahu <yesharshit9@gmail.com>
2026-01-30 04:54:23 +0530

    Update route.ts

--- src/app/api/vibe/route.ts
+++ src/app/api/vibe/route.ts
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
-const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+const apiKey = process.env.GEMINI_API_KEY; 
 const genAI = new GoogleGenerativeAI(apiKey || "");
 
 export async function POST(req: Request) {
